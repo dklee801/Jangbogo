758df7e8eb953c7317ac325f4c0816a6b492eba4
chochonghee <gontry2@naver.com>
2020-08-18 15:34:40 +0900

    product_list 수정_200818

--- jangbogo/urls/jangbogo_urls.py
+++ jangbogo/urls/jangbogo_urls.py
@@ -4,11 +4,11 @@ from jangbogo.views import jangbogo_views
 app_name = 'jangbogo'
 
 urlpatterns = [
-    path('recall_list/', jangbogo_views.recallListAll, name="recallListAll"), #회수.판매중단 리스트 전체 조회
-    path('recall_list/<str:recall_product_nm>/', jangbogo_views.recallListSearch, name="recallListSearch"), #제품명으로 검색 시 회수, 중단 제품 리스트 조회
-    path('recall_list/<int:recall_product_id>/detail/', jangbogo_views.recallListDetail, name="recallListDetail"), #회수.판매중단 제품 상세정보
-    path('product_list/', jangbogo_views.productListAll, name="productListAll"), #제품 리스트 전체 조회
-    path('product_list/<str:product_nm>/', jangbogo_views.productListSearch, name="productListSearch"), #제품명으로 검색 시 제품 리스트 조회
+    #path('recall_list/', jangbogo_views.recallListAll, name="recallListAll"), #회수.판매중단 리스트 전체 조회
+    path('recall_list/', jangbogo_views.recallListSearch, name="recallListSearch"), #제품명으로 검색 시 회수, 중단 제품 리스트 조회
+    path('recall_list/detail/', jangbogo_views.recallListDetail, name="recallListDetail"), #회수.판매중단 제품 상세정보
+   # path('product_list/', jangbogo_views.productListAll, name="productListAll"), #제품 리스트 전체 조회
+    path('product_list/', jangbogo_views.productListSearch, name="productListSearch"), #제품명으로 검색 시 제품 리스트 조회
     path('product_list/<int:product_id>/detail/', jangbogo_views.productListDetail, name="productListDetail"), #제품 상세정보
     path('false_ad_list/', jangbogo_views.falseListAll, name="falseListAll"), #과장.허위광고 제품 리스트 전체 조회
     path('false_ad_list/<str:false_product_nm>/', jangbogo_views.falseListSearch, name="falseListSearch"), #제품명으로 검색 시 과장.허위광고 제품 리스트 조회
--- jangbogo/views/users_views.py
+++ jangbogo/views/users_views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponse
 from jangbogo.models import User, Purchase
 from django.core import serializers
-import json
-import requests
+import json, re, requests
 
 def getServiceKey():
     api_key = "PHjGpBCxONS4DpfJDngspSXPSibgR8%2BWvAcHdqo3zkx%2F3ok5oheb3DR3bpvEfNrTRErQF709yp18hHtVcXnqqg%3D%3D"
@@ -38,7 +37,53 @@ def hasProduct(request, user_id):
     return HttpResponse(json.dumps(context), content_type="application/json")
 
 def addProduct(request, user_id, product_id):
-    return true
+
+    try:
+        user = get_object_or_404(User, pk=user_id)
+
+        pur_name = request.POST["pur_name"]
+       # pur_jejo_num =
+        pur_company = request.POST["pur_company"]
+        pur_date = request.POST["pur_date"]
+        pur_jejodate = request.POST["pur_jejodate"]
+        pur_expire_period = request.POST["pur_expire_period"]
+        pur_jejo_num = request.POST["prdReportNo"]
+
+        # 유통기한 조회 시, 제조인 경우 별도로 처리하기 !!
+        if "년" in pur_expire_period:
+            print("년")
+            expire_period = "".join(re.findall('\d+', pur_expire_period))
+            expire_period = int(expire_period) * 12
+
+            print(expire_period)
+        elif "제조" in pur_expire_period:
+            print("제조로부터")
+            expire_period = "".join(re.findall('\d+', pur_expire_period))
+        else:
+            print("일반")
+            expire_period = "".join(re.findall('\d+', pur_expire_period))
+
+        purchaseObj = Purchase.objects.create (
+            pur_user=user,
+            pur_name=pur_name,
+            pur_company=pur_company,
+            pur_date=pur_date,
+            pur_jejodate=pur_jejodate,
+            pur_expire_period=expire_period,
+            pur_jejo_num=pur_jejo_num
+        )
+
+
+    except (KeyError, User.DoesNotExist):
+        message = '오류가 발생하여 추가할 수 없습니다.'
+    else:
+        purchaseObj.save()
+        message = '보유/구매목록에 추가했습니다.'
+
+    context = {
+        'message': message
+    }
+    return HttpResponse(json.dumps(context), content_type="application/json")
 
 def deleteProduct(request, user_id, product_id):
     return true
