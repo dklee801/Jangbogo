e436c798b604d797c4daac10ae8d428f037aeb71
chochonghee <gontry2@naver.com>
2020-08-20 15:57:51 +0900

    add loading gif in product_list.html _200820

---
+++ static/js/product-detail.js
@@ -0,0 +1,60 @@
+// form - CSRF
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+var csrftoken = getCookie('csrftoken');
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+
+$("form").submit(function(event){
+    event.preventDefault();
+    var user_id = $("input[type=hidden]").val();
+   if (confirm('보유/구매목록에 추가하시겠습니까?')) {
+        // Save it!
+        $.ajax({
+            async : true,
+            url : '/users/'+ user_id +'/add_product/',
+            type : "POST",
+            dataType : "json",
+            beforeSend: function(xhr, settings) {
+                if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                    xhr.setRequestHeader("X-CSRFToken", csrftoken);
+                }
+            },
+            data: {
+                "prdReportNo" : $("#search_prdlst_cd").text(),
+                "pur_name" : $("#search_name").text(),
+                "pur_company" : $("#search_company_nm").text(),
+                "pur_jejodate": $("#jejo_date").val(),
+                "pur_date": $("#pur_date").val(),
+                "pur_expire_period": $("#search_expire_period").text(),
+            },
+            success : function(result) {
+                alert(result.message);
+            },
+            error: function(error){
+                alert(result.message);
+            }
+        });
+    } else {
+        // Do nothing!
+        console.log('Thing was not saved to the database.');
+    }
+});
