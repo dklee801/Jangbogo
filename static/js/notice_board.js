f3166160ef6edbe5b162fa1abc85cb3b51e61220
chochonghee <gontry2@naver.com>
2020-08-24 11:41:04 +0900

    modify recallListFunction in notice_board _200824

--- static/js/notice_board.js
+++ static/js/notice_board.js
@@ -15,6 +15,44 @@ function getExpiredPeriod(dt, expiredDt){
     return dis_day
 }
 
+
+function LoadingWithMask() {
+    //화면의 높이와 너비를 구합니다.
+    var maskHeight = $("#recall-list-container").height();
+    var maskWidth  = $("#recall-list-container").width();
+    var gif = '/static/img/loading_icon.gif'
+
+    //화면에 출력할 마스크를 설정해줍니다.
+    var mask       ="<div id='mask' style='position:absolute; z-index:9000; background-color:#000000; display:flex; align-items: center; justify-content: center;'></div>";
+    var loadingImg ='';
+
+    loadingImg +="<div id='loadingImg'>";
+    loadingImg +=" <img src='"+ gif +"' style=' display: block; margin: 0px auto; margin-top: -50px;'/>";
+    loadingImg +="</div>";
+
+    //화면에 레이어 추가
+    $("#recall-list-container").prepend(mask)
+    $("#mask").append(loadingImg)
+
+    //마스크의 높이와 너비를 화면 것으로 만들어 전체 화면을 채웁니다.
+    $('#mask').css({
+            'width' : maskWidth
+            ,'height': maskHeight
+            ,'opacity' :'0.3'
+    });
+
+    //마스크 표시
+    $('#mask').show();
+
+    //로딩중 이미지 표시
+    $('#loadingImg').show();
+}
+
+function closeLoadingWithMask() {
+    $('#mask, #loadingImg').hide();
+    $('#mask, #loadingImg').remove();
+}
+
 function getHasProductList(){
     var userId = 1;
     var container = $(".has-product-container");
@@ -27,8 +65,9 @@ function getHasProductList(){
             console.log("성공했엉");
             var serviceKey = response.urlInfo.serviceKey
             result = JSON.parse(response.hasProductList)
-            callRecallList(result);
-            callFakeList(result);
+            callRecallList(serviceKey, result);
+            callFakeList(serviceKey, result);
+            callExpiredDate(result);
         },
         error: function(request, status, error){
             console.log("에러났엉");
@@ -37,112 +76,135 @@ function getHasProductList(){
     });
 }
 
-function callFakeList(result){
-$.each(result, function(idx, item){
-                var purName = item.fields.pur_name;
-                var purCompany = item.fields.pur_company;
-                var purDt = item.fields.pur_date;
-                var purJejoDt = item.fields.pur_jejodate;
-                var expirePeriod = item.fields.pur_expire_period;
-     // 허위.과대광고
-                $("#false-ad-tbody").empty()
-                $.ajax({
-                    type:"GET",
-                    url: "http://apis.data.go.kr/1470000/FoodFlshdErtsInfoService/getFoodFlshdErtsList",
-                    data: {
-                        "serviceKey": serviceKey,
-                        "Prduct": purName,
-                        "Entrps": purCompany,
-                    },
-                    success: function(response){
-                        if ($(response).find("item").text() !== ''){
-                            var tr = $("<tr></tr>") // <tr></tr>
-                            var productTd = $("<td></td>").text($(response).find("PRDUCT").text()) // <td></td>
-                            var entrpsTd = $("<td></td>").text($(response).find("ENTRPS").text()) // <td></td>
-                            var dspsDtTd = $("<td></td>").text($(response).find("DSPS_DT").text()) // <td></td>
+// 유통기한 조회
+function callExpiredDate(result){
+    // 유통기한 계산
+    $("#expired-tbody").empty();
+    $.each(result, function(idx, item){
+        var purName = item.fields.pur_name;
+        var purCompany = item.fields.pur_company;
+        var purDt = item.fields.pur_date;
+        var purJejoDt = item.fields.pur_jejodate;
+        var expirePeriod = item.fields.pur_expire_period;
 
-                            tr.append(productTd)
-                            tr.append(entrpsTd)
-                            tr.append(dspsDtTd)
+        var dt = purJejoDt || purDt;
+        var expiredPeriod = getExpiredPeriod(dt, expirePeriod);
 
-                            $("#false-ad-tbody").append(tr)
-                        }
+        if (expiredPeriod < 31) {
+            var tr = $("<tr></tr>") // <tr></tr>
+            var productTd =  $("<td></td>").text(purName) // <td></td>
+            var entrpsTd = $("<td></td>").text(purCompany) // <td></td>
+            var expiredPeriodTd = $("<td></td>").text(expiredPeriod)
 
-                    },
-                    error: function(error){
-                        console.log("에러났엉 ㅠ");
-                        console.log(error)
-                    }
-                })
-        });
+            tr.append(productTd)
+            tr.append(entrpsTd)
+            tr.append(expiredPeriodTd)
+
+            $("#expired-tbody").append(tr)
+        }
+    });
 }
 
-function callRecallList(result){
-            $("#expired-tbody").empty();
-            $.each(result, function(idx, item){
-                var purName = item.fields.pur_name;
-                var purCompany = item.fields.pur_company;
-                var purDt = item.fields.pur_date;
-                var purJejoDt = item.fields.pur_jejodate;
-                var expirePeriod = item.fields.pur_expire_period;
+// 허위.과대광고 & 유통기한 조회
+function callFakeList(serviceKey, result){
+    $("#expired-tbody").empty();
+    $.each(result, function(idx, item){
+        var purName = item.fields.pur_name;
+        var purCompany = item.fields.pur_company;
+        var purDt = item.fields.pur_date;
+        var purJejoDt = item.fields.pur_jejodate;
+        var expirePeriod = item.fields.pur_expire_period;
 
-                // 회수.판매중지
-                $("#recall-tbody").empty()
-                $.ajax({
-                    type:"GET",
-                    url: "http://apis.data.go.kr/1470000/FoodHistInfoService/getAdminRtrvlList",
-                    data: {
-                        "serviceKey": serviceKey,
-                        "pdtnm": purName,
-                    },
-                    success: function(response){
-                        $(response).find("item").each(function(){
-                            var tr = $("<tr></tr>") // <tr></tr>
-                            var productTypeTd =  $("<td></td>").text($(this).find("PDTTYPNM").text()) // <td></td>
-                            var productTd = $("<td></td>").text($(this).find("PDTNM").text()) // <td></td>
-                            var entrpsTd = $("<td></td>").text($(this).find("BRNCHNM").text()) // <td></td>
-                            var mnftDtTd = $("<td></td>").text($(this).find("MNFTDT").text()) // <td></td>
-                            var rclcmtTd = $("<td></td>").text($(this).find("RCLCMT").text()) // <td></td>
-
-                            tr.append(productTypeTd)
-                            tr.append(productTd)
-                            tr.append(entrpsTd)
-                            tr.append(mnftDtTd)
-                            tr.append(rclcmtTd)
-
-                            $("#recall-tbody").append(tr)
-                        })
-
-
-                    },
-                    error: function(error){
-                        console.log("에러났엉 ㅠ");
-                        console.log(error)
-                    }
-                })
-
-                // 유통기한 계산
-                var dt = purJejoDt || purDt;
-                var expiredPeriod = getExpiredPeriod(dt, expirePeriod);
-
-                if (expiredPeriod < 31) {
+        $("#false-ad-tbody").empty();
+        $.ajax({
+            type:"GET",
+            url: "http://apis.data.go.kr/1470000/FoodFlshdErtsInfoService/getFoodFlshdErtsList",
+            data: {
+                "serviceKey": serviceKey,
+                "Prduct": purName,
+                "Entrps": purCompany,
+            },
+            success: function(response){
+                if ($(response).find("item").text() !== ''){
                     var tr = $("<tr></tr>") // <tr></tr>
-                    var productTd =  $("<td></td>").text(purName) // <td></td>
-                    var entrpsTd = $("<td></td>").text(purCompany) // <td></td>
-                    var expiredPeriodTd = $("<td></td>").text(expiredPeriod)
+                    var productTd = $("<td></td>").text($(response).find("PRDUCT").text()) // <td></td>
+                    var entrpsTd = $("<td></td>").text($(response).find("ENTRPS").text()) // <td></td>
+                    var dspsDtTd = $("<td></td>").text($(response).find("DSPS_DT").text()) // <td></td>
 
                     tr.append(productTd)
                     tr.append(entrpsTd)
-                    tr.append(expiredPeriodTd)
+                    tr.append(dspsDtTd)
 
-                    $("#expired-tbody").append(tr)
+                    $("#false-ad-tbody").append(tr)
                 }
+
+            },
+            error: function(error){
+                console.log("에러났엉 ㅠ");
+                console.log(error)
+            }
+        })
+
+        });
+}
+
+
+
+function callRecallList(serviceKey, result){
+    console.log("회수, 판매중지 api 작업중");
+    var keyId ="8c21dcaf0ba44796ada4"
+
+    LoadingWithMask();
+    $("#recall-tbody").empty();
+    $.ajax({
+        type:"GET",
+        url : "http://openapi.foodsafetykorea.go.kr/api/" + keyId +"/I0490/json/1/1000/",
+        success: function(response){
+        noResult = true;
+            $.each(response.I0490.row, function(idx,item) {
+                $.each(result, function(idx, hasProductItem){
+                    var pk = hasProductItem.pk;
+                    var purName = hasProductItem.fields.pur_name;
+                    var purCompany = hasProductItem.fields.pur_company;
+
+                    if (item["PRDTNM"] === purName && item["BSSHNM"].trim() === (purCompany.trim())){
+                        var tr = $("<tr></tr>") // <tr></tr>
+                        var pkTd =  pk
+                        var productTd = $("<td></td>").text(item["PRDTNM"]) // 제품명
+                        var entrpsTd = $("<td></td>").text(item["BSSHNM"]) // 업체명
+                        var mnftDtTd = $("<td></td>").text(item["CRET_DTM"]) // 등록일
+                        var rclcmtTd = $("<td></td>").text(item["RTRVLPRVNS"]) // 회수사유
+
+                        tr.append(pkTd)
+                        tr.append(productTd)
+                        tr.append(entrpsTd)
+                        tr.append(mnftDtTd)
+                        tr.append(rclcmtTd)
+
+                        $("#recall-tbody").append(tr)
+                        noResult = false;
+                    }
+                })
             })
+          if (noResult === true){
+            var tr = $("<tr></tr>");
+            var noResultMsg = $("<h3></h3>").text("검색결과가 없습니다.");
+            tr.append(noResultMsg);
+            $("#tbody_recall").append(tr);
+          }
+
+          closeLoadingWithMask();
+
+        },
+        error: function(error){
+            console.log("에러났엉 ㅠ");
+            console.log(error)
+        }
+    })
 }
 
 function init(){
     getHasProductList();
-    //callRecallList()
 }
 
-init()
+init();
